49845816e8b9081bd8de2c0afb3d7b19dbb4339f
Aniket1675 <Aniket@05>
2022-03-02 22:50:54 +0530

    Completed Day 4 assignment

---
+++ src/com/bridgelabz/snakeandladder/Player.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeandladder;
+public class Player {
+	
+	int position;
+	
+	public Player( int position )
+	{
+		this.position = position;
+	}
+	
+}
+
---
+++ src/com/bridgelabz/snakeandladder/Snake.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeandladder;
+public class Snake {
+	
+	int initialPosition;
+	int finalPosition;
+	
+	public Snake( int initialPosition, int finalPosition )
+	{
+		this.initialPosition = initialPosition;
+		this.finalPosition = finalPosition;
+	}
+}
